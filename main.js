adfd008f32749b1da9af062841ff1b96bcbf3e4b
7220 <kumasaki@naver.com>
2024-09-22 01:07:59 +0900

    uodate

--- main.js
+++ main.js
@@ -159,3 +159,206 @@
 //     }
 //   }
 // }
+
+// var [number, address] = [ 30, 'seoul' ];
+// console.log(number, address)
+// var {address : a , number = 20 } = { address, number };
+// console.log(a, address, number)
+
+
+// let 신체정보 = {
+//   body: {
+//     height: 190,
+//     weight: 70
+//   },
+//   size: ["상의 Large", "바지 30인치"],
+// };
+
+// let {
+//   body: {
+//     height,
+//     weight,
+//   },
+//   size: [ 상의, 하의 ]
+// } = 신체정보;
+
+// console.log(height, weight, 상의, 하의)
+
+// var promise = new Promise(function(resolve, reject) {
+//   var hard = 1 + 1;
+//   reject(hard);
+// });
+
+// promise.then(function(a) {
+//   console.log('success', a)
+// }).catch(function(a) {
+//   console.log('failed', a)
+// })
+
+// var promise = new Promise(function(resolve, reject) {
+//   document.getElementById('test').addEventListener('load', function() {
+//     resolve();
+//   });
+//   document.getElementById('test').addEventListener('error', function() {
+//     reject();
+//   });
+// })
+
+// promise.then(function() {
+//   console.log('success')
+// }).catch(function() {
+//   console.log('failed')
+// })
+
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://codingapple1.github.io/hello.txt', 
+// }).done(function(result) {
+//   // console.log(result)
+// })
+
+// $.ajax({
+//   type: 'GET',
+//   url: 'https://codingapple1.github.io/hello2.txt', 
+// }).done(function(result) {
+//   // console.log(result)
+// })
+
+// var promise1 = new Promise(function(resolve, reject) {
+//   $.get('https://codingapple1.github.io/hello.txt').done(function(result) {
+//     console.log(result)
+//     resolve(result)
+//   }).then(function() {
+//     $.get('https://codingapple1.github.io/hello2.txt').done(function(result2) {
+//       console.log(result2)
+//       resolve(result2)
+//     })
+//   })
+  
+  
+// })
+
+// promise1.then(function(result, result2) {
+//   // console.log(result)
+// }).then(function(result2) {
+//   // console.log(result2)
+// })
+
+
+// var promise2 = new Promise(function( resolve, reject) {
+//   $.get('https://codingapple1.github.io/hello.txt').done(function(result) {
+//     resolve(result)
+//   })
+// })
+
+// promise2.then(function(result) {
+//   console.log(result)
+
+//   var promise3 = new Promise(function(resolve, reject) {
+//     $.get('https://codingapple1.github.io/hello2.txt').done(function(result) {
+//       resolve(result)
+//     })
+//   });
+
+//   return promise3;
+// }).then(function(result) {
+//   console.log(result)
+// })
+
+
+// async function sum() {
+//   var promise4 = new Promise(function(resolve, reject) {
+//     document.getElementById('test2').addEventListener('click', function() {
+//       resolve('성공 ~');
+//     })
+//   })
+//   try {
+//     var result4 = await promise4;
+//     console.log(result4)
+//   } catch {
+//     console.log('뭐함?')
+//   }
+
+// }
+
+// sum()
+
+
+// var obj = { name : 'kim', age : 20 };
+
+// for (const key in obj) {
+//   console.log(obj[key])
+// }
+
+// let 데이터 = [1,2,3,4,5,6,7,8,9] 
+
+// for (let i of 데이터) {
+//   for (let j of 데이터) {
+//     console.log(`${i} x ${j} = ${i * j}`)
+//   }
+// }
+
+
+// var products = [
+//   {
+//     name1 : 'chair',
+//     price1 : 7000,
+//   },
+//   {
+//     name2 : 'sofa',
+//     price : 5000,
+//   },
+//   {
+//     name1 : 'desk',
+//     price3 : 9000,
+//   },
+// ];
+// var reg = /[^0-9]/g;
+
+// for (var i of products) {
+//   // 배열안에 오브젝트의 키 중 오타부분인 숫자를 없앤다
+//   console.log(i)
+//   for ( var j in i) {
+//     console.log(j)
+    
+//   }
+// }
+
+// let newValue, newKey
+
+
+// for (var i of products) {
+//   for ( var j in i) {
+//     if (isNaN(parseInt(j.slice(-1))) == false) {
+//       newValue = i[j];
+//       newKey = j.slice(0, -1);
+//       i[newKey] = newValue;    
+//       delete i[j]   
+      
+//     }
+//   }
+// }
+
+// // console.log(products)
+
+// var symbol = Symbol('???')
+// var weight = Symbol('secret')
+// var height = Symbol('small')
+
+// var person = {
+//   name : 'kim'
+// }
+// person.weight = 200
+// person[weight] = 250
+// person.height = 190
+// person[height] = 130
+// console.log(weight,height)
+
+// for (var i in person) {
+//   console.log(person[i])
+// }
+
+var person = new Map();
+
+person.set('name', 'kim')
+console.log(person)
